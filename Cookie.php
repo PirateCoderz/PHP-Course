aff8d3f660c9f525fa5e7eb4e056b00d4fb7aba3
PirateCoderz <galpha964@gmail.com>
2023-08-12 11:33:38 +0500

    learning cookies

--- Cookie.php
+++ Cookie.php
@@ -5,9 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Cookie Practice</title>
 </head>
 <body>
-<?php ?><br>
+
+<h1 style="text-align: center">Cookie Time</h1>
+
+
+<?php
+
+$ExpireTime=time() + 60;
+echo "Current Time is: ". $ExpireTime. "<br>";
+setcookie("Name", "Pirate Coderz", $ExpireTime);
+setcookie("Age", 21, $ExpireTime);
+
+echo $_COOKIE["Name"]."<br>";
+
+
+?><br>
 </body>
 </html>
